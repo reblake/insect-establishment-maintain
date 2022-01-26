1b83781488caded7b9f692f062a47f969c5dfcb3
Rmturner24 <rmturner24@gmail.com>
2022-01-26 17:57:55 +1300

    testing make_occurrence_table script

--- temporary_insectcleanr_functions.R
+++ temporary_insectcleanr_functions.R
@@ -2,6 +2,9 @@
 # temporary file to load current insectcleanr functions before package gets updated.
 
 # functions copied from insectcleanr package main branch 26 Jan 2022
+# and the separate_source function from the source_table branch
+# and coalesce_manual
+# and coalesce_occur - this file has disappeared from the recent update to the main branch, found it in the old custom functions file
 
 #################################################
 ####################################################################
@@ -424,5 +427,209 @@ separate_attributes_csv <- function(df_location){
 
 
 
+###################################
+# from the source_table branch
 
+###########################################################
+#' separate_source_csv: function to read in and separate source info using csv files
+#'
+#' @param df_location location of files containing taxa info
+#'
+#' @import dplyr
+#'
+#' @return dataframe
+#' @export
+#'
+#' @examples
+separate_source_csv <- function(df_location){ # RT renamed function
+  # reads the excel file in
+  df <- read.csv(df_location, strip.white = TRUE)
+  
+  # clean up column names, capitalization, etc.
+  df_1 <- df %>%
+    # replace " " and "." with "_" in column names
+    select_all(~gsub("\\s+|\\.", "_", .)) %>%
+    select_all(tolower) %>%  # make all column names lower case
+    mutate_all(~gsub("\\b([[:upper:]])([[:upper:]]+)",
+                     "\\U\\1\\L\\2", . , perl=TRUE)) %>%
+    mutate_all(~gsub("\\.", "", . , perl=TRUE))
+  
+  # define region
+  file_name <- sapply(strsplit(as.character(df_location), split="/") , function(x) x[5])
+  country_nm <- sapply(strsplit(as.character(file_name), split="_") , function(x) x[1])
+  
+  # define what relevant columns to select - note that the original column caled genus_species will become the user_supplied_name column
+  source_columns <- c("genus_species", "year","intentional_release","eradicated","established_indoors_or_outdoors","source")
+  
+  # split off any columns with any taxonomic column names
+  df_2 <- df_1 %>%
+    select(tidyselect::one_of(source_columns)) %>%
+    # add the name of the country as a column
+    mutate(region = country_nm) %>%
+    mutate_all(~gsub("(*UCP)\\s\\+|\\W+$", "", . , perl=TRUE)) %>%
+    # replace any non-numerical values in year column with NA
+    mutate(year = gsub("u", NA_character_, year, perl=TRUE)) %>%
+    mutate(genus_species = gsub("\\ssp\\s[a-z]+\\s[a-z]+$", "", genus_species, perl=TRUE),
+           genus_species = gsub("\\ssp\\s[A-Za-z]+\\d+$", "", genus_species, perl=TRUE),
+           genus_species = gsub("\\ssp\\.\\d\\s\\s[A-Za-z]+$", "", genus_species, perl=TRUE),
+           genus_species = gsub("\\s[A-Z]\\.[A-Z]\\.[A-Z][a-z]+\\,\\s\\d+$", "", genus_species, perl=TRUE),
+           genus_species = gsub("[^\x20-\x7E]sp", "", genus_species, perl=TRUE),
+           genus_species = gsub("[^\x20-\x7E]", " ", genus_species, perl=TRUE),
+           genus_species = gsub("\\s\\([^()]*\\)", "\\1", genus_species, perl=TRUE),
+           genus_species = gsub("\\([A-Z].*$", "\\1", genus_species, perl=TRUE),
+           genus_species = gsub("^([A-Z][a-z]+\\s\\S+).*", "\\1", genus_species, perl=TRUE),
+           genus_species = gsub("\\sssp\\.\\s[a-z].*$", "", genus_species, perl=TRUE),
+           genus_species = gsub("\\ssp\\.[A-Z]$", "", genus_species, perl=TRUE),
+           genus_species = gsub("\\ssp[A-Z]$", "", genus_species, perl=TRUE),
+           genus_species = gsub("\\ssp(\\.|p|\\d|\\.\\d)$", "", genus_species, perl=TRUE),
+           genus_species = gsub("\\.", "", genus_species, perl=TRUE),
+           genus_species = gsub("\\sn\\.sp\\.$", "", genus_species, perl=TRUE),
+           genus_species = gsub("\\sn$", "", genus_species, perl=TRUE),
+           genus_species = gsub("\\s\\ssp$", "", genus_species, perl=TRUE),
+           genus_species = gsub("\\d+$", "", genus_species, perl=TRUE),
+           genus_species = gsub("\\s\\ss", " ", genus_species, perl=TRUE),
+           genus_species = gsub("\\s\\s", " ", genus_species, perl=TRUE),
+           genus_species = gsub("\\ssp$", "", genus_species, perl=TRUE)
+    )
+  
+  # return df_2
+  return(df_2)
+}
 
+######################################################
+
+#######################################################
+#' coalesce_manual: function to manually coalesce rows in attribute table
+#'
+#' @param df dataframe that may need coalescing
+#'
+#' @import dplyr
+#' @importFrom DescTools Mode
+#'
+#' @return dataframe
+#' @export
+#'
+#' @examples
+#' file <- system.file("extdata", "Japan_taxa.xlsx", package = "insectcleanr", mustWork = TRUE)
+#' data_j <- read_excel(file)
+#' data_co <- coalesce_manual(data_j)
+coalesce_manual <- function(df) {
+  # test whether there are multiple rows
+  if(nrow(df) == 1){coal_manual <- df %>%
+    mutate_at(vars(taxon_id, genus_species, plant_feeding,
+                   intentional_release, ever_introduced_anywhere,
+                   host_type, established_indoors_or_outdoors, host_group,
+                   phagy, pest_type, ecozone, phagy_main,
+                   current_distribution_cosmopolitan_, feeding_type, feeding_main,
+                   confirmed_establishment),
+              list(as.character)) %>%
+    mutate_at(vars(origin_Nearctic, origin_Neotropic, origin_European_Palearctic,
+                   origin_Asian_Palearctic, origin_Indomalaya, origin_Afrotropic,
+                   origin_Australasia, origin_Oceania),
+              list(as.numeric))
+  } else {
+    # coalesce non-origin columns
+    coal_other <- df %>%
+      select(taxon_id, genus_species, plant_feeding, intentional_release, ever_introduced_anywhere,
+             host_type, established_indoors_or_outdoors, host_group, phagy, pest_type,
+             ecozone, current_distribution_cosmopolitan_, phagy_main, feeding_type, feeding_main,
+             confirmed_establishment) %>%
+      group_by(genus_species) %>%
+      summarize_all(DescTools::Mode, na.rm = TRUE) %>%
+      ungroup()
+    
+    # coalesce origin columns
+    coal_origin <- df %>%
+      select(genus_species, starts_with("origin_")) %>%
+      mutate_at(vars(origin_Nearctic, origin_Neotropic, origin_European_Palearctic,
+                     origin_Asian_Palearctic, origin_Indomalaya, origin_Afrotropic,
+                     origin_Australasia, origin_Oceania),
+                list(as.numeric)) %>%
+      group_by(genus_species) %>%
+      summarize_all( ~ ifelse((sum(., na.rm = TRUE) %in% c(1:10)), 1, 0)) %>%
+      ungroup()
+    
+    # bind together origin and non-origin columns
+    coal_manual <- full_join(coal_other, coal_origin) %>%
+      select(genus_species, origin_Nearctic, origin_Neotropic, origin_European_Palearctic,
+             origin_Asian_Palearctic, origin_Indomalaya, origin_Afrotropic,
+             origin_Australasia, origin_Oceania, plant_feeding, intentional_release,
+             ever_introduced_anywhere, everything()) %>%
+      mutate_at(vars(taxon_id, genus_species, plant_feeding,
+                     intentional_release, ever_introduced_anywhere,
+                     host_type, established_indoors_or_outdoors, host_group,
+                     phagy, pest_type, ecozone, phagy_main,
+                     current_distribution_cosmopolitan_, feeding_type, feeding_main,
+                     confirmed_establishment),
+                list(as.character)) %>%
+      mutate_at(vars(origin_Nearctic, origin_Neotropic, origin_European_Palearctic,
+                     origin_Asian_Palearctic, origin_Indomalaya, origin_Afrotropic,
+                     origin_Australasia, origin_Oceania),
+                list(as.numeric))
+  }
+  
+  return(coal_manual)
+  
+}
+
+########################################
+#######################################################
+#' Title: function to manually coalesce rows in occurrence table   
+#'
+#' @param df 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+coalesce_occur <- function(df) {  
+  # test whether there are multiple rows
+  if(nrow(df) == 1) {no_dup <- df %>% 
+    select(genus_species, year, region, country, origin, 
+           host_type, ecozone, intentional_release, 
+           established_indoors_or_outdoors, confirmed_establishment, eradicated,
+           present_status) %>% 
+    mutate_at(vars(genus_species, region, country, 
+                   host_type, origin, ecozone, intentional_release,
+                   established_indoors_or_outdoors, confirmed_establishment, 
+                   eradicated, present_status), 
+              list(as.character)) %>% 
+    mutate_at(vars(year), list(as.numeric))
+  } else {
+    
+    # coalesce to earliest year
+    yr <- df %>%
+      select(genus_species, region, year) %>%
+      group_by(genus_species, region) %>%
+      # summarize_all( ~ ifelse(nrow(year)>1 , , year)) %>%
+      filter(rank(year, ties.method = "first") == 1) %>% 
+      ungroup() %>% 
+      mutate_at(vars(year), list(as.numeric))
+    
+    # take out duplicates in the genus_species/region columns               
+    gsr_dp <- df %>% 
+      select(-year) %>% 
+      group_by(genus_species, region) %>% 
+      summarize_all(DescTools::Mode, na.rm = TRUE) %>% 
+      ungroup() %>% 
+      select(genus_species, region, country, origin, 
+             host_type, ecozone, intentional_release, 
+             established_indoors_or_outdoors, confirmed_establishment, eradicated,
+             present_status) %>% 
+      mutate_at(vars(genus_species, region, country, 
+                     host_type, origin, ecozone, intentional_release,
+                     established_indoors_or_outdoors, confirmed_establishment, 
+                     eradicated, present_status),
+                list(as.character)) 
+    
+    # put year and everything else back together
+    no_dup <- gsr_dp %>% 
+      full_join(yr) %>% 
+      select(genus_species, year, region, country, origin, 
+             host_type, ecozone, intentional_release, 
+             established_indoors_or_outdoors, confirmed_establishment, eradicated,
+             present_status)
+    
+  }
+  return(no_dup)
+}        
