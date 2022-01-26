8d6265824fbd0f760310e9fa1a150cbd3da0370f
Rmturner24 <rmturner24@gmail.com>
2022-01-27 11:26:41 +1300

    updating functions

--- temporary_insectcleanr_functions.R
+++ temporary_insectcleanr_functions.R
@@ -1,10 +1,10 @@
 #################################################
 # temporary file to load current insectcleanr functions before package gets updated.
 
-# functions copied from insectcleanr package main branch 26 Jan 2022
-# and the separate_source function from the source_table branch
+# functions copied from insectcleanr package main branch 27 Jan 2022
+
 # and coalesce_manual
-# and coalesce_occur - this file has disappeared from the recent update to the main branch, found it in the old custom functions file
+# and coalesce_occur 
 
 #################################################
 ####################################################################
@@ -426,77 +426,11 @@ separate_attributes_csv <- function(df_location){
 }
 
 
+####################################################################
+##### Functions for manipulating taxonomic and related info    #####
+##### originally created by Rachael Blake    04/11/2019        #####
+####################################################################
 
-###################################
-# from the source_table branch
-
-###########################################################
-#' separate_source_csv: function to read in and separate source info using csv files
-#'
-#' @param df_location location of files containing taxa info
-#'
-#' @import dplyr
-#'
-#' @return dataframe
-#' @export
-#'
-#' @examples
-separate_source_csv <- function(df_location){ # RT renamed function
-  # reads the excel file in
-  df <- read.csv(df_location, strip.white = TRUE)
-  
-  # clean up column names, capitalization, etc.
-  df_1 <- df %>%
-    # replace " " and "." with "_" in column names
-    select_all(~gsub("\\s+|\\.", "_", .)) %>%
-    select_all(tolower) %>%  # make all column names lower case
-    mutate_all(~gsub("\\b([[:upper:]])([[:upper:]]+)",
-                     "\\U\\1\\L\\2", . , perl=TRUE)) %>%
-    mutate_all(~gsub("\\.", "", . , perl=TRUE))
-  
-  # define region
-  file_name <- sapply(strsplit(as.character(df_location), split="/") , function(x) x[5])
-  country_nm <- sapply(strsplit(as.character(file_name), split="_") , function(x) x[1])
-  
-  # define what relevant columns to select - note that the original column caled genus_species will become the user_supplied_name column
-  source_columns <- c("genus_species", "year","intentional_release","eradicated","established_indoors_or_outdoors","source")
-  
-  # split off any columns with any taxonomic column names
-  df_2 <- df_1 %>%
-    select(tidyselect::one_of(source_columns)) %>%
-    # add the name of the country as a column
-    mutate(region = country_nm) %>%
-    mutate_all(~gsub("(*UCP)\\s\\+|\\W+$", "", . , perl=TRUE)) %>%
-    # replace any non-numerical values in year column with NA
-    mutate(year = gsub("u", NA_character_, year, perl=TRUE)) %>%
-    mutate(genus_species = gsub("\\ssp\\s[a-z]+\\s[a-z]+$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp\\s[A-Za-z]+\\d+$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp\\.\\d\\s\\s[A-Za-z]+$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\s[A-Z]\\.[A-Z]\\.[A-Z][a-z]+\\,\\s\\d+$", "", genus_species, perl=TRUE),
-           genus_species = gsub("[^\x20-\x7E]sp", "", genus_species, perl=TRUE),
-           genus_species = gsub("[^\x20-\x7E]", " ", genus_species, perl=TRUE),
-           genus_species = gsub("\\s\\([^()]*\\)", "\\1", genus_species, perl=TRUE),
-           genus_species = gsub("\\([A-Z].*$", "\\1", genus_species, perl=TRUE),
-           genus_species = gsub("^([A-Z][a-z]+\\s\\S+).*", "\\1", genus_species, perl=TRUE),
-           genus_species = gsub("\\sssp\\.\\s[a-z].*$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp\\.[A-Z]$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp[A-Z]$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp(\\.|p|\\d|\\.\\d)$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\.", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\sn\\.sp\\.$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\sn$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\s\\ssp$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\d+$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\s\\ss", " ", genus_species, perl=TRUE),
-           genus_species = gsub("\\s\\s", " ", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp$", "", genus_species, perl=TRUE)
-    )
-  
-  # return df_2
-  return(df_2)
-}
-
-######################################################
 
 #######################################################
 #' coalesce_manual: function to manually coalesce rows in attribute table
@@ -572,7 +506,8 @@ coalesce_manual <- function(df) {
   
 }
 
-########################################
+
+
 #######################################################
 #' Title: function to manually coalesce rows in occurrence table   
 #'
@@ -633,3 +568,6 @@ coalesce_occur <- function(df) {
   }
   return(no_dup)
 }        
+
+
+
